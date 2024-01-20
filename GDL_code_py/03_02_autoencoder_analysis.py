818f5bf9245cfd8b11d782f817d13d621d9013a0
StuartGavidia <stuarta0403@hotmail.com>
2024-01-19 19:06:37 -0800

    Allow discord bot to generate number images 0-9 using VAE model

--- GDL_code_py/03_02_autoencoder_analysis.py
+++ GDL_code_py/03_02_autoencoder_analysis.py
@@ -50,27 +50,36 @@ AE = load_model(Autoencoder, RUN_FOLDER)
 # In[ ]:
 
 
-n_to_show = 10
-example_idx = np.random.choice(range(len(x_test)), n_to_show)
-example_images = x_test[example_idx]
+# Specify the digits you want to reconstruct
+desired_digits = [5]
 
+# Find the indices of those digits in the test set
+indices = []
+for digit in desired_digits:
+    idx = np.where(y_test == digit)[0]
+    indices.append(idx[0])  # Take the first occurrence for simplicity
+
+# Select the images for these indices
+example_images = x_test[indices]
+
+# The rest of the code remains the same
 z_points = AE.encoder.predict(example_images)
-
 reconst_images = AE.decoder.predict(z_points)
 
+# Visualization code
 fig = plt.figure(figsize=(15, 3))
 fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
-for i in range(n_to_show):
+for i in range(len(indices)):
     img = example_images[i].squeeze()
-    ax = fig.add_subplot(2, n_to_show, i+1)
+    ax = fig.add_subplot(2, len(indices), i+1)
     ax.axis('off')
     ax.text(0.5, -0.35, str(np.round(z_points[i],1)), fontsize=10, ha='center', transform=ax.transAxes)   
     ax.imshow(img, cmap='gray_r')
 
-for i in range(n_to_show):
+for i in range(len(indices)):
     img = reconst_images[i].squeeze()
-    ax = fig.add_subplot(2, n_to_show, i+n_to_show+1)
+    ax = fig.add_subplot(2, len(indices), i+len(indices)+1)
     ax.axis('off')
     ax.imshow(img, cmap='gray_r')
 
@@ -80,23 +89,23 @@ for i in range(n_to_show):
 # In[ ]:
 
 
-n_to_show = 5000
-grid_size = 15
-figsize = 12
+# n_to_show = 5000
+# grid_size = 15
+# figsize = 12
 
-example_idx = np.random.choice(range(len(x_test)), n_to_show)
-example_images = x_test[example_idx]
-example_labels = y_test[example_idx]
+# example_idx = np.random.choice(range(len(x_test)), n_to_show)
+# example_images = x_test[example_idx]
+# example_labels = y_test[example_idx]
 
-z_points = AE.encoder.predict(example_images)
+# z_points = AE.encoder.predict(example_images)
 
-min_x = min(z_points[:, 0])
-max_x = max(z_points[:, 0])
-min_y = min(z_points[:, 1])
-max_y = max(z_points[:, 1])
+# min_x = min(z_points[:, 0])
+# max_x = max(z_points[:, 0])
+# min_y = min(z_points[:, 1])
+# max_y = max(z_points[:, 1])
 
-plt.figure(figsize=(figsize, figsize))
-plt.scatter(z_points[:, 0] , z_points[:, 1], c='black', alpha=0.5, s=2)
+#plt.figure(figsize=(figsize, figsize))
+#plt.scatter(z_points[:, 0] , z_points[:, 1], c='black', alpha=0.5, s=2)
 plt.show()
 
 
@@ -105,96 +114,97 @@ plt.show()
 # In[ ]:
 
 
-figsize = 5
+# figsize = 5
 
-plt.figure(figsize=(figsize, figsize))
-plt.scatter(z_points[:, 0] , z_points[:, 1], c='black', alpha=0.5, s=2)
+# plt.figure(figsize=(figsize, figsize))
+# plt.scatter(z_points[:, 0] , z_points[:, 1], c='black', alpha=0.5, s=2)
 
-grid_size = 10
-grid_depth = 3
-figsize = 15
+# grid_size = 10
+# grid_depth = 3
+# figsize = 15
 
-x = np.random.uniform(min_x,max_x, size = grid_size * grid_depth)
-y = np.random.uniform(min_y,max_y, size = grid_size * grid_depth)
-z_grid = np.array(list(zip(x, y)))
-reconst = AE.decoder.predict(z_grid)
+# x = np.random.uniform(min_x,max_x, size = grid_size * grid_depth)
+# y = np.random.uniform(min_y,max_y, size = grid_size * grid_depth)
+# z_grid = np.array(list(zip(x, y)))
+# reconst = AE.decoder.predict(z_grid)
 
-plt.scatter(z_grid[:, 0] , z_grid[:, 1], c = 'red', alpha=1, s=20)
-plt.show()
+# plt.scatter(z_grid[:, 0] , z_grid[:, 1], c = 'red', alpha=1, s=20)
+# plt.show()
 
-fig = plt.figure(figsize=(figsize, grid_depth))
-fig.subplots_adjust(hspace=0.4, wspace=0.4)
+# fig = plt.figure(figsize=(figsize, grid_depth))
+# fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
-for i in range(grid_size*grid_depth):
-    ax = fig.add_subplot(grid_depth, grid_size, i+1)
-    ax.axis('off')
-    ax.text(0.5, -0.35, str(np.round(z_grid[i],1)), fontsize=10, ha='center', transform=ax.transAxes)
+# for i in range(grid_size*grid_depth):
+#     ax = fig.add_subplot(grid_depth, grid_size, i+1)
+#     ax.axis('off')
+#     ax.text(0.5, -0.35, str(np.round(z_grid[i],1)), fontsize=10, ha='center', transform=ax.transAxes)
     
-    ax.imshow(reconst[i, :,:,0], cmap = 'Greys')
+#     ax.imshow(reconst[i, :,:,0], cmap = 'Greys')
 
 
 # In[ ]:
 
 
-n_to_show = 5000
-grid_size = 15
-figsize = 12
+# n_to_show = 5000
+# grid_size = 15
+# figsize = 12
 
-example_idx = np.random.choice(range(len(x_test)), n_to_show)
-example_images = x_test[example_idx]
-example_labels = y_test[example_idx]
+# example_idx = np.random.choice(range(len(x_test)), n_to_show)
+# example_images = x_test[example_idx]
+# example_labels = y_test[example_idx]
 
-z_points = AE.encoder.predict(example_images)
+# z_points = AE.encoder.predict(example_images)
 
-plt.figure(figsize=(figsize, figsize))
-plt.scatter(z_points[:, 0] , z_points[:, 1] , cmap='rainbow' , c= example_labels
-            , alpha=0.5, s=2)
-plt.colorbar()
-plt.show()
+# plt.figure(figsize=(figsize, figsize))
+# plt.scatter(z_points[:, 0] , z_points[:, 1] , cmap='rainbow' , c= example_labels
+#             , alpha=0.5, s=2)
+# plt.colorbar()
+# plt.show()
 
 
 # In[ ]:
 
 
-n_to_show = 5000
-grid_size = 20
-figsize = 8
+# n_to_show = 5000
+# grid_size = 20
+# figsize = 8
 
-example_idx = np.random.choice(range(len(x_test)), n_to_show)
-example_images = x_test[example_idx]
-example_labels = y_test[example_idx]
+# example_idx = np.random.choice(range(len(x_test)), n_to_show)
+# example_images = x_test[example_idx]
+# example_labels = y_test[example_idx]
 
-z_points = AE.encoder.predict(example_images)
+# z_points = AE.encoder.predict(example_images)
 
-plt.figure(figsize=(5, 5))
-plt.scatter(z_points[:, 0] , z_points[:, 1] , cmap='rainbow' , c= example_labels
-            , alpha=0.5, s=2)
-plt.colorbar()
+# plt.figure(figsize=(5, 5))
+# plt.scatter(z_points[:, 0] , z_points[:, 1] , cmap='rainbow' , c= example_labels
+#             , alpha=0.5, s=2)
+# plt.colorbar()
 
 # x = norm.ppf(np.linspace(0.05, 0.95, 10))
 # y = norm.ppf(np.linspace(0.05, 0.95, 10))
-x = np.linspace(min(z_points[:, 0]), max(z_points[:, 0]), grid_size)
-y = np.linspace(max(z_points[:, 1]), min(z_points[:, 1]), grid_size)
-xv, yv = np.meshgrid(x, y)
-xv = xv.flatten()
-yv = yv.flatten()
-z_grid = np.array(list(zip(xv, yv)))
 
-reconst = AE.decoder.predict(z_grid)
+# x = np.linspace(min(z_points[:, 0]), max(z_points[:, 0]), grid_size)
+# y = np.linspace(max(z_points[:, 1]), min(z_points[:, 1]), grid_size)
+# xv, yv = np.meshgrid(x, y)
+# xv = xv.flatten()
+# yv = yv.flatten()
+# z_grid = np.array(list(zip(xv, yv)))
 
-plt.scatter(z_grid[:, 0] , z_grid[:, 1], c = 'black'#, cmap='rainbow' , c= example_labels
-            , alpha=1, s=5)
+# reconst = AE.decoder.predict(z_grid)
 
-
-
-
-plt.show()
+# plt.scatter(z_grid[:, 0] , z_grid[:, 1], c = 'black'#, cmap='rainbow' , c= example_labels
+#             , alpha=1, s=5)
 
 
-fig = plt.figure(figsize=(figsize, figsize))
-fig.subplots_adjust(hspace=0.4, wspace=0.4)
-for i in range(grid_size**2):
-    ax = fig.add_subplot(grid_size, grid_size, i+1)
-    ax.axis('off')
-    ax.imshow(reconst[i, :,:,0], cmap = 'Greys')
+
+
+# plt.show()
+
+
+# fig = plt.figure(figsize=(figsize, figsize))
+# fig.subplots_adjust(hspace=0.4, wspace=0.4)
+# for i in range(grid_size**2):
+#     ax = fig.add_subplot(grid_size, grid_size, i+1)
+#     ax.axis('off')
+#     ax.imshow(reconst[i, :,:,0], cmap = 'Greys')
 
--- test.py
+++ test.py
@@ -2,11 +2,19 @@ import nest_asyncio
 import random
 nest_asyncio.apply()
 import discord
+
+from utils.loaders import load_mnist, load_model
+from models.AE import Autoencoder
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
 intents = discord.Intents.default()
 intents.message_content = True
-TOKEN = " MTE3OTE1MjU2NDg0ODE3MzA2Ng.GCEuC4.ro3pKi-xlqPviduSux6lk5ZeaJ-cIF15rqcSWo" # you
-#should copy your token for your bot
+TOKEN = " " #  token for bot
+
 client = discord.Client(intents=intents)
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
@@ -25,6 +33,8 @@ async def on_member_join(member):
 
     #if message.author == client.user:
         #return
+    
+
 @client.event
 async def on_message(message):
     bot_quotes = [
@@ -52,10 +62,59 @@ async def on_message(message):
             await message.channel.send(f"Hey {person_name}: The result is: {result}")
         except Exception as e:
             await message.channel.send(f"Error in calculation: {e}")
-    elif message.content.startswith("Generate person"):
-        #generate image
+    elif message.content.startswith("generate number"):
+        message_content = message.content.lower().strip()  
+        parts = message_content.split()
+        if len(parts) >= 3 and parts[1].lower() == "number" and parts[2].isdigit():
+            digit = int(parts[2]) 
+        else:
+            digit = 1  
 
-        #send image back
-        await message.channel.send(f"test")
+        #ensure the digit is within the valid range for MNIST (0-9)
+        if digit not in range(10):
+            await message.channel.send("Please provide a valid digit (0-9).")
+            return
+
+        SECTION = 'vae'
+        RUN_ID = '0001'
+        DATA_NAME = 'digits'
+        RUN_FOLDER = 'run/{}/'.format(SECTION)
+        RUN_FOLDER += '_'.join([RUN_ID, DATA_NAME])
+
+        (x_train, y_train), (x_test, y_test) = load_mnist()
+
+        AE = load_model(Autoencoder, RUN_FOLDER)
+
+        desired_digits = [digit]
+
+        indices = []
+        for d in desired_digits:  
+            idx = np.where(y_test == d)[0]
+            indices.append(idx[0])  
+
+        example_images = x_test[indices]
+
+        z_points = AE.encoder.predict(example_images)
+        reconst_images = AE.decoder.predict(z_points)
+
+        fig = plt.figure(figsize=(15, 3))
+        fig.subplots_adjust(hspace=0.4, wspace=0.4)
+        fig = plt.figure(figsize=(5, 5)) 
+
+        for i in range(len(indices)):
+            img = reconst_images[i].squeeze()
+            ax = fig.add_subplot(1, len(indices), i+1)
+            ax.axis('off')
+            ax.imshow(img, cmap='gray_r')
+
+        plt.tight_layout() 
+
+        image_path = os.path.join(RUN_FOLDER, f"images/generated_number_{digit}.png")
+        fig.savefig(image_path)
+        plt.close(fig)
+
+        with open(image_path, 'rb') as f:
+            picture = discord.File(f)
+            await message.channel.send(file=picture)
         
 client.run(TOKEN)
