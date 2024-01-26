0a87b564e0b32be76d8685834a719958f2de5374
StuartGavidia <stuarta0403@hotmail.com>
2024-01-25 22:06:49 -0800

    Readjust parameters

--- discordbot.py
+++ discordbot.py
@@ -270,9 +270,9 @@ async def generate_gan_person(message):
 
 async def generate_chat_response(message):
     model = load_model_keras('./saved_models/aesop_dropout_100.h5')
-    next_words = 200
+    next_words = 500
     temp = 0.2
-    max_sequence_len = 5 
+    max_sequence_len = 1
     start_story = '| ' * max_sequence_len
 
     seed_text = message.content
